e7460c21defc802cb2446b428a4734ebc27899b8
Daniel Berger <djberg96@gmail.com>
2013-01-04 05:24:20 -0700

    Fixed a regex for darwin.

--- lib/sys/admin.rb
+++ lib/sys/admin.rb
@@ -2,16 +2,16 @@
 require 'rbconfig'
 
 case RbConfig::CONFIG['host_os']
-when /linux/i
-  require 'linux/sys/admin'
-when /sunos|solaris/i
-  require 'sunos/sys/admin'
-when /cygwin|mingw|mswin|windows|dos/i
-  require 'windows/sys/admin'
-when /darin|mach/i
-  require 'darwin/sys/admin'
-when /bsd/i
-  require 'bsd/sys/admin'
-else
-  require 'unix/sys/admin'
+  when /linux/i
+    require 'linux/sys/admin'
+  when /sunos|solaris/i
+    require 'sunos/sys/admin'
+  when /cygwin|mingw|mswin|windows|dos/i
+    require 'windows/sys/admin'
+  when /darwin|mach/i
+    require 'darwin/sys/admin'
+  when /bsd/i
+    require 'bsd/sys/admin'
+  else
+    require 'unix/sys/admin'
 end
