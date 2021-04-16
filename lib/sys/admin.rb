3137fb7376a7b52920c8e4d6d2f57e5dfcacbeb5
Daniel Berger <djberg96@gmail.com>
2021-04-16 13:23:30 -0400

    Align when with case.

--- lib/sys/admin.rb
+++ lib/sys/admin.rb
@@ -9,15 +9,15 @@ end
 require 'rbconfig'
 
 case RbConfig::CONFIG['host_os']
-  when /linux/i
+when /linux/i
     require 'linux/sys/admin'
-  when /sunos|solaris/i
+when /sunos|solaris/i
     require 'sunos/sys/admin'
-  when /cygwin|mingw|mswin|windows|dos/i
+when /cygwin|mingw|mswin|windows|dos/i
     require 'windows/sys/admin'
-  when /darwin|mach/i
+when /darwin|mach/i
     require 'darwin/sys/admin'
-  when /bsd/i
+when /bsd/i
     require 'bsd/sys/admin'
   else
     require 'unix/sys/admin'
