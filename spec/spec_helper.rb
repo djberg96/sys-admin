05287e97e9185cf53a3b3fb0f89fd3166f3b718c
Daniel Berger <djberg96@gmail.com>
2021-09-25 08:24:31 -0400

    Add unix and windows filter to spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,6 @@ require 'sys-admin'
 
 RSpec.configure do |config|
   config.filter_run_excluding(:darwin) if Gem::Platform.local.os != 'darwin'
+  config.filter_run_excluding(:windows) unless Gem.win_platform?
+  config.filter_run_excluding(:unix) if Gem.win_platform?
 end
