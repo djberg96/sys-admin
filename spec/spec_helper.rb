848401ab1106c04a9eed1b4f48c1aa526d05f475
Daniel Berger <djberg96@gmail.com>
2021-09-25 08:30:32 -0400

    Put Windows handling directly in spec_helper.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,10 @@ require 'sys-admin'
 RSpec.configure do |config|
   config.filter_run_excluding(:darwin) if Gem::Platform.local.os != 'darwin'
   config.filter_run_excluding(:windows) unless Gem.win_platform?
-  config.filter_run_excluding(:unix) if Gem.win_platform?
+
+  if Gem.win_platform?
+    config.filter_run_excluding(:unix)
+    require 'win32-security'
+    require 'socket'
+  end
 end
