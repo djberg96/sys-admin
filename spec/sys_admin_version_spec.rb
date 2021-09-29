65376ddd6c2d54686ddb10488cad508d3f3344df
Daniel Berger <djberg96@gmail.com>
2021-09-29 08:56:19 -0400

    Add spec for constructor.

--- spec/sys_admin_version_spec.rb
+++ spec/sys_admin_version_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Sys::Admin do
   example "version constant is frozen" do
     expect(described_class::VERSION).to be_frozen
   end
+
+  example "constructor is private" do
+    expect{ described_class.new }.to raise_error(NoMethodError)
+  end
 end
